5fc95eee5532040d9aa520ef06e267133aaa221c
Thekiborg <none>
2024-04-06 16:09:06 +0200

    Added empty solution

---
+++ 1.5/Source/Moyo2.cs
@@ -0,0 +1,12 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Reflection;
+global using RimWorld;
+global using Verse;
+global using UnityEngine;
+global using HarmonyLib;
+
+namespace Moyo2
+{
+
+}
