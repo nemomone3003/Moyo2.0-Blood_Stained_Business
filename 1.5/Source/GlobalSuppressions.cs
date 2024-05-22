bcd625aedbd9d37dbd651ca5ede756057eaecfa7
Thekiborg <none>
2024-05-22 12:01:18 +0200

    Fixed linkable being able to be built offset and fixed linkables' area

--- 1.5/Source/GlobalSuppressions.cs
+++ 1.5/Source/GlobalSuppressions.cs
@@ -10,3 +10,4 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:Moyo2.ModExtension_FishTank.FishDefs")]
 [assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:Moyo2.WorkGiver_EmptyFishTank.ShouldSkip(Verse.Pawn,System.Boolean)~System.Boolean")]
 [assembly: SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "<Pending>", Scope = "type", Target = "~T:Moyo2.Moyo2")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:Moyo2.PlaceWorker_OnBuilding.AllowsPlacing(Verse.BuildableDef,Verse.IntVec3,Verse.Rot4,Verse.Map,Verse.Thing,Verse.Thing)~Verse.AcceptanceReport")]
