c8ebf8f3364f0164b85828bbc7d834cedc8ffebb
Thekiborg <doflr415@gmail.com>
2025-07-09 17:47:56 +0200

    Made the extracters replace each other when installing

---
+++ 1.5/Source/Moyo2/CompProperties/CompProperties_InstallImplantReplace.cs
@@ -0,0 +1,14 @@
+﻿namespace Moyo2
+{
+#pragma warning disable CA1051
+	public class CompProperties_InstallImplantReplace : CompProperties_UseEffectInstallImplant
+	{
+		public CompProperties_InstallImplantReplace()
+		{
+			compClass = typeof(Comp_InstallImplantReplace);
+		}
+
+
+		public Dictionary<HediffDef, ThingDef> incompatibleImplants;
+	}
+}
---
+++ 1.6/Source/Moyo2/Thing/Comps/Comp_InstallImplantReplace.cs
@@ -0,0 +1,22 @@
+﻿namespace Moyo2
+{
+	public class Comp_InstallImplantReplace : CompUseEffect_InstallImplant
+	{
+		public new CompProperties_InstallImplantReplace Props => (CompProperties_InstallImplantReplace)props;
+
+
+		public override void DoEffect(Pawn user)
+		{
+			for (int i = user.health.hediffSet.hediffs.Count - 1; i >= 0; i--)
+			{
+				Hediff hediff = user.health.hediffSet.hediffs[i];
+				if (Props.incompatibleImplants.TryGetValue(hediff.def, out ThingDef implantThingDef))
+				{
+					user.health.RemoveHediff(hediff);
+					GenSpawn.Spawn(implantThingDef, user.Position, user.Map);
+				}
+			}
+			base.DoEffect(user);
+		}
+	}
+}
