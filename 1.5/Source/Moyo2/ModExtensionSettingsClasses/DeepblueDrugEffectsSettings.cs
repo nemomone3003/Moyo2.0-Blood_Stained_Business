3997607bd25da012360c7bedc9f76aad7677dff8
Nemonian <88618961+nemomone3003@users.noreply.github.com>
2026-06-15 19:45:59 +0900

    1.6 release prep. applied body retex

--- 1.5/Source/Moyo2/ModExtensionSettingsClasses/DeepblueDrugEffectsSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Moyo2
-{
-	public class DeepblueDrugEffectsSettings
-	{
-		public bool isDeepblueDrug;
-		public List<int> moodOffsetsGivenPerStage;
-	}
-}
--- 1.5/Source/Moyo2/WorkGiver/WorkGiver_MoyoDeepDrill.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Moyo2
-{
-	public class WorkGiver_MoyoDeepDrill : WorkGiver_DeepDrill
-	{
-		public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(Moyo2_ThingDefOfs.Moyo2_DeepDrill);
-	}
-}
